af76a5683a4c85b7749fb1e80cbcf2d4d8d4b03d
Md Aquib Raza <razaaquib99@gmail.com>
2026-04-03 00:09:50 +0530

    Switch fallback to SQLite

--- cloth_store/settings.py
+++ cloth_store/settings.py
@@ -83,10 +83,11 @@ WSGI_APPLICATION = 'cloth_store.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 import dj_database_url
+import os
 
 DATABASES = {
     'default': dj_database_url.config(
-        default='mysql://clothuser:Cloth@123@localhost:3306/cloth_store_db',
+        default='sqlite:///' + str(BASE_DIR / 'db.sqlite3'),
         conn_max_age=600,
         conn_health_checks=True,
     )
